c982fa8b4eaa63190634b4576975aac88df8bee8
ZinouRefsi12 <refsizinou7@gmail.com>
2025-01-17 00:34:28 +0100

    Add files via upload

---
+++ Personnel/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import personnel , service 
+admin.site.register(personnel)
+admin.site.register(service) 
+                        
