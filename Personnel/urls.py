f38d289ed980f444c5a6da52b70f6b6379c6d786
ZinouRefsi12 <refsizinou7@gmail.com>
2025-01-17 14:59:23 +0100

    Update urls.py

--- Personnel/urls.py
+++ Personnel/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),  
-   # path('conge/', views.conges, name='conges'),
+    path('personnel/', views.liste_personnel, name='personnel'),  
+    path('employe/', views.employe, name='employe'),
+     path('ajouteremploye/', views.ajouter_employe, name='ajouter_employe'),
     
 ] 
